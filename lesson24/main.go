518d151f8e21b27af59b1a62d04ec3c868b38d04
roaris <one0803kyopro@gmail.com>
2022-01-06 22:50:48 +0900

    lesson24

--- lesson24/main.go
+++ lesson24/main.go
@@ -18,16 +18,14 @@ func main() {
 		}
 	*/
 
-	/*
-		switch n := 2; n {
-		case 1, 2:
-			fmt.Println("1 or 2")
-		case 3, 4:
-			fmt.Println("3 or 4")
-		default:
-			fmt.Println("I don't know")
-		}
-	*/
+	switch n := 2; n {
+	case 1, 2:
+		fmt.Println("1 or 2")
+	case 3, 4:
+		fmt.Println("3 or 4")
+	default:
+		fmt.Println("I don't know")
+	}
 
 	/*
 		n := 6
