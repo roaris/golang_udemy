81f1979d7b90750523ce338a5ac70db345baedd5
roaris <one0803kyopro@gmail.com>
2022-01-06 21:23:33 +0900

    lesson11

---
+++ lesson11/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+// 定数
+
+const Pi = 3.14
+
+const (
+	URL = "http://xxx.co.jp"
+	SiteName = "test"
+)
+
+const (
+	A = 1
+	B
+	C
+	D = "A"
+	E
+	F
+)
+
+// var Big int = 9223372036854775807 + 1
+const Big = 9223372036854775807 + 1
+
+const (
+	c0 = iota
+	c1
+	c2
+)
+
+func main() {
+	fmt.Println(Pi)
+
+	// Pi = 3
+	// fmt.Println(Pi)
+
+	fmt.Println(URL, SiteName)
+
+	fmt.Println(A, B, C, D, E, F)
+
+	fmt.Println(Big - 1)
+
+	fmt.Println(c0, c1, c2)
+}
