e7ef08f3bc340af71bbb7a623a2e55db9b43a1c6
roaris <one0803kyopro@gmail.com>
2022-01-06 21:35:53 +0900

    lesson14

---
+++ lesson14/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// 論理演算子
+
+func main() {
+	fmt.Println(true && false == true)
+	fmt.Println(true && true == true)
+	fmt.Println(true && false == false)
+
+	fmt.Println(true || false == true)
+	fmt.Println(false || false == true)
+
+	fmt.Println(!true)
+	fmt.Println(!false)
+}
